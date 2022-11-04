9769a6d9992cad254ccfd8a1d3ba9457901e9b76
Luna-tan7 <115689817+Luna-tan7@users.noreply.github.com>
2022-11-03 23:16:53 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 (function () {
 
-    var names = ["Yaakov", "Jorge", "Juana", "Jerry", "Pedro", "Figueroa", "Luis", "Paola", "Luisa", "Jose"];
-    
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  
     for (var i = 0; i < names.length; i++) {
       var firstLetter = names[i].charAt(0).toLowerCase();
     
